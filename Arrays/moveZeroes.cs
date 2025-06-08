1fd6543ef8c46f14c9e2f5ea896e4f6bbc2582bd
SaiDeepthiKonanki <saicse.rymec@gmail.com>
2025-06-08 19:56:16 +1000

    Create moveZeroes.cs

---
+++ Arrays/moveZeroes.cs
@@ -0,0 +1,23 @@
+/*
+Move Zeros
+Example 1:
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Example 2:
+Input: nums = [0]
+Output: [0]
+*/
+public class moveZeroes {
+    public void moveZero(int[] nums) {
+        int i = 0; 
+        for (int j = 0; j < nums.Length; j++) {
+            if (nums[j] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+            }
+        }
+    }
+}
