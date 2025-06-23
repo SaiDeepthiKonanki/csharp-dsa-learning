68f1bca16a8a8be53033f7113bde7bdd30079a3c
SaiDeepthiKonanki <saicse.rymec@gmail.com>
2025-06-23 22:40:03 +1000

    Create reverseString.cs

---
+++ Strings/reverseString.cs
@@ -0,0 +1,14 @@
+/* Reverse a String
+Example 1:
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+
+Example 2:
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+*/
+public class reverseString {
+    public void reverseString(char[] s) {
+        Array.Reverse(s);
+    }
+}
